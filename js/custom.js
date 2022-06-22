52107f3323715ab6f79762203aa50c58b6615bd1
Aman Hussain <amnhussain8910@gmail.com>
2022-06-22 17:57:30 +0530

    changes

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.counter-box h4 span').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
